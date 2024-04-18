8d0b870b7b0122a6c04cc3a6939c2ff5bb53de18
nouhaila <154752182+nouha-ech@users.noreply.github.com>
2024-04-18 06:14:55 +0100

    Add files via upload

---
+++ etoile.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+int i,j,N;   
+printf("donner  un entier : ");
+scanf("%d",&N);
+
+for(i=0;i<N;i++){
+  for(j=1;j<=(N*2)-1;j++){
+    if (j>=N-i && j<= N+i )                    
+        printf("*");
+      else   printf(" ");   
+  }
+  printf("\n");      
+  }
+return 0 ;    
+}
